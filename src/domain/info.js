9a8902fca830fe0124505d549d45e5a27b4ef1ec
Hezekiah <hezekiahmaina3@gmail.com>
2020-06-03 10:35:18 +0300

    Implemented the main functionality of getting info about the domain with samba-tool

--- src/domain/info.js
+++ src/domain/info.js
@@ -1,30 +1,82 @@
 import cockpit from 'cockpit';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { RenderError, Loading } from '../common';
+import {
+    Form,
+    FormGroup,
+    TextInput,
+    Button,
+    Card,
+    CardBody,
+    CardHeader,
+    ActionGroup,
+} from '@patternfly/react-core';
 
 export default function DomainInfo() {
-    // const [info, setInfo] = useState({});
-    const [ipAddress, setIPAdress] = useState('127.0.0.1');
+    const [info, setInfo] = useState();
+    const [ipAddress, setIPAdress] = useState();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+    const [alertVisible, setAlertVisible] = useState(false);
 
-    const command = `samba-tool domain info ${ipAddress}`;
+    const hideAlert = () => {
+        setAlertVisible(false);
+    };
 
-    const getInfo = () => cockpit.script(command, { superuser: true, err: 'message' });
+    const handleIPAddressChange = (e) => {
+        setIPAdress(e);
+    };
 
-    useEffect(() => {
-        getInfo()
-                .done((data) => {
-                    // setInfo(data);
-                    console.log('Done');
-                    console.log(data);
+    const getDomainInfo = (e) => {
+        e.preventDefault();
+        setLoading(true);
+        const command = `samba-tool domain info ${ipAddress}`;
+        const script = () => cockpit.script(command, { superuser: true, err: "message" })
+                .then((data) => {
+                    setInfo(data);
+                    setLoading(false);
                 })
                 .catch((exception) => {
-                    console.log('Exception');
-                    console.log(exception);
+                    if (exception != null) {
+                        setError(exception.message);
+                        setAlertVisible(true);
+                        setLoading(false);
+                    }
                 });
-    }
-    );
+        return script();
+    };
+
     return (
-        <div>
-            <h1>Main Domain Component</h1>
-        </div>
+        <>
+            <div>
+                <Form isHorizontal onSubmit={getDomainInfo}>
+                    <FormGroup
+                    label="IP Address"
+                    isRequired
+                    fieldId="horizontal-form-ip-address"
+                    helperText="Enter an IP Address"
+                    >
+                        <TextInput
+                        value={ipAddress}
+                        type="text"
+                        id="horizontal-form-ip-address"
+                        aria-describedby="horizontal-form-ip-address-helper"
+                        name="horizontal-form-ip-address"
+                        onChange={handleIPAddressChange}
+                        placeholder="127.0.0.1"
+                        />
+                    </FormGroup>
+                    <ActionGroup>
+                        <Button variant="primary" type="submit">Submit</Button>
+                    </ActionGroup>
+                </Form>
+                <Card isHoverable>
+                    <CardHeader>Domain Info</CardHeader>
+                    <CardBody>{info}</CardBody>
+                </Card>
+                <Loading loading={loading} />
+                <RenderError error={error} hideAlert={hideAlert} alertVisible={alertVisible} />
+            </div>
+        </>
     );
 }
