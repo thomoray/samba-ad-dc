075d34065391971db7e043040159a29762ed3f49
Hezekiah <hezekiahmaina3@gmail.com>
2020-06-03 10:30:48 +0300

    Added common.js for Components that will be reused in the app

---
+++ src/common.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+    Alert,
+    AlertActionCloseButton,
+    Spinner
+} from '@patternfly/react-core';
+
+export const RenderError = (props) => {
+    const hideAlert = props.hideAlert;
+    if (props.alertVisible) {
+        return (
+            <Alert
+            variant="danger"
+            title="An Error Occurred"
+            action={<AlertActionCloseButton onClose={hideAlert} />}
+            >
+                {props.error}
+            </Alert>
+        );
+    }
+    return <div />;
+};
+
+export const Loading = (props) => {
+    if (props.loading) {
+        return (
+            <div>
+                <Spinner />
+            </div>
+        );
+    }
+    return <div />;
+};
