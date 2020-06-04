f05a977410750c3ecf7e0b67395cd0ecbebb7b3d
Hezekiah <hezekiahmaina3@gmail.com>
2020-06-04 17:42:37 +0300

    Removed the unnecessary cockpit import, div to style the child Components of Computer

--- src/computer/index.js
+++ src/computer/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './list';
-import cockpit from 'cockpit';
+import Show from './show';
+import Create from './create';
+import Delete from './delete';
 import './css/computer.css';
 
 export default function Computer() {
     return (
         <div>
-            <h1 className="heading-1">Main Computer Management Component</h1>
-            <List />
+            <h1 className="heading-1">Computer Management</h1>
+            <div className="components">
+                <List />
+            </div>
+            <div className="components">
+                <Show />
+            </div>
+            <div className="components">
+                <Create />
+            </div>
+            <div className="components">
+                <Delete />
+            </div>
         </div>
     );
 }
