5eaf2f610542e3c6b23210ea223437a20561a397
Hezekiah <hezekiahmaina3@gmail.com>
2020-06-12 10:25:13 +0300

    Added a way to filter and sort computers in the list

--- src/computer/list.js
+++ src/computer/list.js
@@ -5,7 +5,12 @@ import {
     Card,
     CardBody,
     CardFooter,
-    CardHeader
+    CardHeader,
+    InputGroup,
+    Button,
+    ButtonVariant,
+    SearchIcon,
+    TextInput
 } from '@patternfly/react-core';
 import './css/computer.css';
 
@@ -14,16 +19,27 @@ function ComputerList() {
     const [error, setError] = useState();
     const [loading, setLoading] = useState(true);
     const [alertVisible, setAlertVisible] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+
+    const onSearchInputChange = (value) => setSearchValue(value);
 
     const hideAlert = () => {
         setAlertVisible(false);
     };
 
+    const filteredList = list.filter((computer) => computer.includes(searchValue)).map(filteredComputer =>
+        <li key={filteredComputer.toString()}>
+            {filteredComputer}
+        </li>
+    );
+
     useEffect(() => {
         const command = `samba-tool computer list`;
         const script = () => cockpit.script(command, { superuser: true, err: 'message' })
                 .done((data) => {
-                    setList(data);
+                    const splitData = data.split('\n');
+                    const sortedData = splitData.sort();
+                    setList(sortedData);
                     setLoading(false);
                 })
                 .catch((exception) => {
@@ -38,11 +54,27 @@ function ComputerList() {
         <>
             <div>
                 <h3 className="list-computer-heading">Computers in AD DC</h3>
+                <InputGroup>
+                    <TextInput
+                    name="computerSearchInput"
+                    id="computerSearchInput"
+                    type="search"
+                    aria-label="search computers"
+                    onChange={onSearchInputChange}
+                    value={searchValue}
+                    />
+                    <Button
+                    variant={ButtonVariant.control}
+                    aria-label="search button for search computers"
+                    >
+                        <SearchIcon />
+                    </Button>
+                </InputGroup>
                 <Card>
                     <CardHeader>Computer List</CardHeader>
                     <CardBody>
                         <Loading loading={loading} />
-                        <p>{list}</p>
+                        {filteredList}
                     </CardBody>
                     <CardFooter>
                         <RenderError hideAlert={hideAlert} error={error} alertVisible={alertVisible} />
